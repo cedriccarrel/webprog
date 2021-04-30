4d45eb96e2c8ecfc19c694451064e634cf410909
cedriccarrel <cedi_16@hotmail.com>
2021-04-30 19:03:14 +0200

    V1 - api

--- demos/server.py
+++ demos/server.py
@@ -1,7 +1,25 @@
 from flask import Flask, render_template
 import json
+from nba_api.stats.endpoints import commonplayerinfo #pip install nba_api
 
 app = Flask(__name__)
+
+# Basic Request
+player_info = commonplayerinfo.CommonPlayerInfo(player_id=2544)
+
+
+custom_headers = {
+    'Host': 'stats.nba.com',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:61.0) Gecko/20100101 Firefox/61.0',
+    'Accept': 'application/json, text/plain, */*',
+    'Accept-Language': 'en-US,en;q=0.5',
+    'Referer': 'https://stats.nba.com/',
+    'Accept-Encoding': 'gzip, deflate, br',
+    'Connection': 'keep-alive',
+}
+# Only available after v1.1.0
+# Proxy Support, Custom Headers Support, Timeout Support (in seconds)
+player_info = commonplayerinfo.CommonPlayerInfo(player_id=2544, proxy='127.0.0.1:80', headers=custom_headers, timeout=100)
 
 
 @app.route('/')
@@ -18,8 +36,11 @@ def schreibe_daten_in_json(pfad, daten):
     with open(pfad, 'w') as datei:
         json.dump(daten, datei, indent = 4)
 
+
+
 def index():
 	return render_template('/index.html')
+
 
 
 
